e156ca1426ec648a2c43d3237148bd2b49782677
Tech-F <franmiguezformal@hotmail.com>
2023-02-12 14:12:48 +0100

    Empezando a confirgurar tipo de vencimiento

---
+++ src/UsuarioLogin/Proveedores/Tipo.java
@@ -0,0 +1,11 @@
+package UsuarioLogin.Proveedores;
+
+public class Tipo {
+    private String eleccion;
+
+    public Tipo(String eleccion) {
+        case = eleccion{
+
+        }
+    }
+}
--- src/UsuarioLogin/Proveedores/Vencimientos.java
+++ src/UsuarioLogin/Proveedores/Vencimientos.java
@@ -1,4 +1,7 @@
 package UsuarioLogin.Proveedores;
 
 public class Vencimientos {
+    //TODO: desde la fecha que se marque, +  días que marques aquí.
+    private int days;
+    private Tipo type;
 }
