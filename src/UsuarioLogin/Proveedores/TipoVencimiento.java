6f0487d1fef7b1aeeb844b79948afe0c8c84efb9
Tech-F <franmiguezformal@hotmail.com>
2023-02-12 14:14:49 +0100

    hay ue hacer el case

---
+++ src/UsuarioLogin/Proveedores/TipoVencimiento.java
@@ -0,0 +1,13 @@
+package UsuarioLogin.Proveedores;
+
+public class TipoVencimiento extends Vencimientos{
+    private String eleccion;
+
+    public TipoVencimiento(String eleccion) {
+        switch ( eleccion){
+            case Giro:
+
+
+        }
+    }
+}
--- src/UsuarioLogin/Proveedores/Vencimientos.java
+++ src/UsuarioLogin/Proveedores/Vencimientos.java
@@ -3,5 +3,5 @@ package UsuarioLogin.Proveedores;
 public class Vencimientos {
     //TODO: desde la fecha que se marque, +  días que marques aquí.
     private int days;
-    private Tipo type;
+    private TipoVencimiento type;
 }
